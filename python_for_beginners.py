f47e3d87f4df03835acafd2273494fbc6d769394
JOSE <diazvjosetomas@gmail.com>
2023-04-29 10:20:36 -0400

    first example

---
+++ python_for_beginners.py
@@ -0,0 +1,37 @@
+# Programadores de Software
+# w3pdsoft@gmail.com
+# w3pds.com
+# Linkeding Ing Jose Diaz
+# https://www.linkedin.com/in/diazvjosetomas/
+# Linkeding Programadores de Software
+# https://www.linkedin.com/company/w3pds/
+# Definir una lista de números
+
+numeros = [10, 20, 30, 40]
+
+# Definir una función que calcula la suma de una lista
+
+def suma(lista):
+
+  # Inicializar una variable para almacenar el resultado
+
+  resultado = 0
+
+  # Recorrer cada elemento de la lista
+
+  for elemento in lista:
+
+    # Sumar el elemento al resultado
+
+    resultado = resultado + elemento
+
+  # Devolver el resultado
+
+  return resultado
+
+
+
+# Llamar a la función con la lista de números y mostrar el resultado
+
+print("La suma de los números es:", suma(numeros))
+
